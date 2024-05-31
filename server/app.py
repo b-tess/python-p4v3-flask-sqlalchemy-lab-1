b2491a2b78da781f98ef080d28634bd2aaa63fff
Barbara Tess <barbaratessn@gmail.com>
2024-05-31 15:14:32 +0300

    Assignment complete

--- server/app.py
+++ server/app.py
@@ -21,6 +21,39 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter_by(id = id).first()
+
+    if earthquake:
+        body = earthquake.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+    
+    return make_response(body, status)
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def eathquakes_by_magnitude(magnitude):
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    earthquakes_list = []
+
+    if len(earthquakes) > 0:
+        for earthquake in earthquakes:
+            earthquakes_list.append(earthquake.to_dict())
+
+        body = {
+            'count': len(earthquakes),
+            'quakes': earthquakes_list
+        }
+    else:
+        body = {
+            'count': len(earthquakes),
+            'quakes': earthquakes_list
+        }
+
+    return make_response(body, 200)
 
 
 if __name__ == '__main__':
